64d317ebf6a7c3516cac1dd4dc87f853940a7b0c
aishmano <>
2014-02-08 21:11:18 +0600

    Redesign

--- src/main/java/kz/supershiny/core/model/Tire.java
+++ src/main/java/kz/supershiny/core/model/Tire.java
@@ -19,7 +19,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import org.hibernate.annotations.Cascade;
 
 /**
  *
